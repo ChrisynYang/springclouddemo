ee39ed942679e44a50fefa0dd5bdce96d7864656
root <260496076@qq.com>
2018-10-27 11:14:30 +0800

    add log

--- eurekaserver/src/main/java/com/SpringcloudexaApplication.java
+++ eurekaserver/src/main/java/com/SpringcloudexaApplication.java
@@ -2,14 +2,21 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringcloudexaApplication {
+public class SpringcloudexaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         System.out.println("EurekaServer running");
         SpringApplication.run(SpringcloudexaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringcloudexaApplication.class);
     }
 }
